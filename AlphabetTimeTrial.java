1fc4a1866906d15c9a63384b109eaef4bd2fed7e
odalydarragh1 <156336154+odalydarragh1@users.noreply.github.com>
2025-01-23 13:47:39 +0000

    Add files via upload

---
+++ AlphabetTimeTrial.java
@@ -0,0 +1,63 @@
+import static java.lang.Character.*;
+import java.util.Scanner;
+
+public class AlphabetTimeTrial {
+    private final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public AlphabetTimeTrial() {
+        beginTrial(getDirection());
+    }
+
+    public void beginTrial(boolean backwards) {
+        Scanner in = new Scanner(System.in);
+        char input;
+
+        int i = 0; if (backwards) {i=25;}
+        int incriment = 1; if (backwards) {incriment = -1;}
+
+        System.out.println("First letter: " + alphabet[i]);
+        long timer = System.nanoTime();
+        while (i <= 25 && i >= 0) {
+            input = toUpperCase(in.next().charAt(0));
+
+            if (input == alphabet[i]) {
+                System.out.print("Correct!");
+                i += incriment;
+            } else {
+                System.out.print("Incorrect!");
+            }
+            
+            if (i!=-1 && i!=26){
+                System.out.print("\tNext letter: " + alphabet[i] + "\n");
+            }
+        }
+        timer = (System.nanoTime() - timer)/1000000000;
+        System.out.println("\tTime taken: " + timer + "s");
+    }
+
+    public boolean getDirection() {
+        boolean valid = false;
+        char direction;
+        boolean backwards = false;
+        Scanner input = new Scanner(System.in);
+
+        while (!valid) {
+            System.out.println("Enter the direction: F/B");
+            direction = toUpperCase(input.next().charAt(0));
+
+            if (direction == 'B') {
+                valid = true;
+                backwards = true;
+            }
+
+            else if (direction == 'F') {
+                valid = true;
+            }
+
+            else {
+                System.out.println("Invalid direction");
+            }
+        }
+        return backwards;
+    }
+}
